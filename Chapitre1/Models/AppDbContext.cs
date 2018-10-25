b3bfbe8fa90408480bbb6aa0d8597db691445709
elkabir <mokbir@yahoo.fr>
2018-10-25 12:28:52 +0200

    Ajout Models

---
+++ Chapitre1/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Chapitre1.Models
+{
+    public class AppDbContext: DbContext
+    {
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Entities/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Author
+    {
+        public int AurhorId { get; set; }
+        public string Name { get; set; }
+        public string WebUrl { get; set; }
+    }
+}
